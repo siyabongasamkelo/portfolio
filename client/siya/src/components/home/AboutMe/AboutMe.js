c58393f2021160fc868f0de3468efdf36d0900cb
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-12 15:25:47 -0700

    chore: aboutMe folder with files

---
+++ client/siya/src/components/home/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import { H3 } from "../Home.styled";
+import { AboutMeText, AboutMeWrapper } from "./AboutMe.styled";
+
+const AboutMe = () => {
+  return (
+    <AboutMeWrapper>
+      <AboutMeText>
+        <H3>About Me</H3>
+      </AboutMeText>
+    </AboutMeWrapper>
+  );
+};
+
+export default AboutMe;
---
+++ client/siya/src/components/home/AboutMe/AboutMe.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const AboutMeWrapper = styled.section``;
+
+export const AboutMeText = styled.div``;
