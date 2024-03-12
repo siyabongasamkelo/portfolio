1084c87fba0c7051284582b7ef996cd69bd7dd6d
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-12 14:06:57 -0700

    test: commented everything else & left header to focus on header

--- client/siya/src/components/home/Home.js
+++ client/siya/src/components/home/Home.js
@@ -8,19 +8,23 @@ import {
   ImageCover,
   H1,
   H3,
+  P,
 } from "./Home.styled";
 import Hero from "../img/Hero.png";
+import { MyButton } from "../header/Header.styled";
 
 const Home = () => {
   return (
     <HomeWrapper>
       <Header />
-      <HomeContainer>
+      {/* <HomeContainer>
         <TextContainer>
           <div>
             <H3>I am</H3>
             <H1>Siyabonga Mazibuko</H1>
-            <H3>The web developer</H3>
+            <H3>
+              <span>The web developer</span>
+            </H3>
           </div>
         </TextContainer>
         <ImageContainer>
@@ -28,8 +32,20 @@ const Home = () => {
             <img src={Hero} alt="hero" />
           </ImageCover>
         </ImageContainer>
-        <ActionContainer></ActionContainer>
-      </HomeContainer>
+        <ActionContainer>
+          <div>
+            <P>
+              I am a very <span>passionate</span> web developer who specializes
+              in <span>javaScript</span>&nbsp;and is always working hard
+              <span>everyday</span> to improve his skills
+            </P>
+            <div>
+              <MyButton>My Projects</MyButton>
+              <MyButton>DownLoad Cv</MyButton>
+            </div>
+          </div>
+        </ActionContainer>
+      </HomeContainer> */}
     </HomeWrapper>
   );
 };
