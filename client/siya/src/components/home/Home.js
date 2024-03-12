9ef1477809f888199e990a9c3be1e53e9815ef2c
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-12 13:05:17 -0700

    feat: added textContainer, ImageContainer & Action container

--- client/siya/src/components/home/Home.js
+++ client/siya/src/components/home/Home.js
@@ -1,11 +1,35 @@
 import Header from "../header/Header";
-import { HomeContainer, HomeWrapper } from "./Home.styled";
+import {
+  ActionContainer,
+  HomeContainer,
+  HomeWrapper,
+  ImageContainer,
+  TextContainer,
+  ImageCover,
+  H1,
+  H3,
+} from "./Home.styled";
+import Hero from "../img/Hero.png";
 
 const Home = () => {
   return (
     <HomeWrapper>
       <Header />
-      <HomeContainer></HomeContainer>
+      <HomeContainer>
+        <TextContainer>
+          <div>
+            <H3>I am</H3>
+            <H1>Siyabonga Mazibuko</H1>
+            <H3>The web developer</H3>
+          </div>
+        </TextContainer>
+        <ImageContainer>
+          <ImageCover>
+            <img src={Hero} alt="hero" />
+          </ImageCover>
+        </ImageContainer>
+        <ActionContainer></ActionContainer>
+      </HomeContainer>
     </HomeWrapper>
   );
 };
