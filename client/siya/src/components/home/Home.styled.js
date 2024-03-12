178f75ac78cd855aa492690bd0069e5d1200806e
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-12 13:06:09 -0700

    feat: styled home components

--- client/siya/src/components/home/Home.styled.js
+++ client/siya/src/components/home/Home.styled.js
@@ -8,15 +8,39 @@ export const HomeContainer = styled.div`
 
 export const TextContainer = styled.div`
   width: 30%;
-  background-color: red;
+  display: flex;
+  align-items: center;
 `;
 
 export const ImageContainer = styled.div`
   width: 40%;
-  background-color: green;
+`;
+
+export const ImageCover = styled.div`
+  height: 70vh;
+  width: 70%;
+  margin-left: 15%;
+  margin-top: 10%;
+  background-color: #ecb365;
+  border-radius: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    object-fit: cover;
+    filter: brightness(85%);
+  }
 `;
 
 export const ActionContainer = styled.div`
   width: 30%;
-  background-color: blue;
+`;
+
+export const H1 = styled.h1`
+  color: rgba(225, 225, 225, 0.9);
+  font-size: 74px;
+`;
+
+export const H3 = styled.h3`
+  color: rgba(225, 225, 225, 0.9);
 `;
