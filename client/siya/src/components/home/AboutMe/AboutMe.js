a3f4d733cbd5c50da63d386b682b97af4bb48a21
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-13 13:23:19 -0700

    feat: added more info about me & my skills

--- client/siya/src/components/home/AboutMe/AboutMe.js
+++ client/siya/src/components/home/AboutMe/AboutMe.js
@@ -1,11 +1,68 @@
-import { H3 } from "../Home.styled";
-import { AboutMeText, AboutMeWrapper } from "./AboutMe.styled";
+import { H1, H3, P } from "../Home.styled";
+import { AboutMEPara, AboutMeText, AboutMeWrapper } from "./AboutMe.styled";
 
 const AboutMe = () => {
   return (
     <AboutMeWrapper>
       <AboutMeText>
-        <H3>About Me</H3>
+        <H1>About Me</H1>
+        <AboutMEPara>
+          <H3>How i got into programming</H3>
+          <P>
+            My first language was visual basic back in 2019 when i was in
+            college ( Umfolozi TVET College ), i loved it so much i was doing
+            projects everyday, learned about :
+          </P>
+          <ul>
+            <li>Data types</li>
+            <li>Arrays</li>
+            <li>classes & Objects</li>
+            <li>OOP (Object Orientated Programming)</li>
+            <li>Database (My Sql)</li>
+          </ul>
+        </AboutMEPara>
+        <AboutMEPara>
+          <P>
+            Then fast forward to 2021 i started pursuing web development and i
+            never looked back i learned :
+          </P>
+          <ul>
+            <li>Html </li>
+          </ul>
+          <ul>
+            <li>Css</li>
+          </ul>
+          <P>i then pursued javascript in javascripti learned:</P>
+          <ul>
+            <li>Arrays</li>
+          </ul>
+          <ul>
+            <li>Promises</li>
+          </ul>
+          <ul>
+            <li>Callbacks</li>
+          </ul>
+          <ul>
+            <li>Arrow functions</li>
+          </ul>
+          <ul>
+            <li>async/await</li>
+          </ul>
+        </AboutMEPara>
+        <AboutMEPara>
+          <H3>javascript Frameworks & Libraries</H3>
+          <P>
+            After coding in javascript for months i wanted to add extra
+            functionalities to my websites like storing data in Database and
+            maybe autheticating users so i learned nodejs :
+          </P>
+          <ul>
+            <li>Nodejs</li>
+          </ul>
+          <ul>
+            <li>Npm</li>
+          </ul>
+        </AboutMEPara>
       </AboutMeText>
     </AboutMeWrapper>
   );
