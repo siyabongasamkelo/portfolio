9eb339666b738b740947bd10c21f803e60553ac9
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-13 13:25:19 -0700

    feat: added paragraph cover div with li & h3 styles

--- client/siya/src/components/home/AboutMe/AboutMe.styled.js
+++ client/siya/src/components/home/AboutMe/AboutMe.styled.js
@@ -2,4 +2,18 @@ import styled from "styled-components";
 
 export const AboutMeWrapper = styled.section``;
 
-export const AboutMeText = styled.div``;
+export const AboutMeText = styled.div`
+  margin-top: 20%;
+`;
+
+export const AboutMEPara = styled.div`
+  width: 35%;
+  margin-left: 10%;
+  li {
+    color: rgba(225, 225, 225, 0.9);
+  }
+  h3 {
+    margin-top: 5%;
+    margin-bottom: 5%;
+  }
+`;
