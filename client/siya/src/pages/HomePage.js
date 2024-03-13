27787ae32a2e755288adcb41b4d8490eefc5beb4
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-13 16:34:19 -0700

    feat: added MyProjects Section

--- client/siya/src/pages/HomePage.js
+++ client/siya/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import AboutMe from "../components/home/AboutMe/AboutMe";
 import Home from "../components/home/Home";
+import MyProjects from "../components/myprojects/MyProjects";
 
 const HomePage = () => {
   return (
     <>
       <Home />
       <AboutMe />
+      <MyProjects />
     </>
   );
 };
