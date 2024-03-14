a099bd1f2c5cc7bc6a36a6fcb2cb795706fdd65d
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-14 13:21:01 -0700

    feat: changed the entire home page

--- client/siya/src/components/home/Home.js
+++ client/siya/src/components/home/Home.js
@@ -1,15 +1,14 @@
 import Header from "../header/Header";
 import {
-  ActionContainer,
   HomeContainer,
   HomeWrapper,
-  ImageContainer,
-  TextContainer,
-  ImageCover,
   H1,
   H3,
   P,
-  ButtonsCover,
+  TextContainer,
+  Circle,
+  ButtonContainer,
+  ParagraphContainer,
 } from "./Home.styled";
 import Hero from "../img/Hero.png";
 import { MyButton } from "../header/Header.styled";
@@ -19,33 +18,27 @@ const Home = () => {
     <HomeWrapper>
       <Header />
       <HomeContainer>
+        <Circle>
+          <img src={Hero} alt="siya" />
+        </Circle>
         <TextContainer>
-          <div>
-            <H3>I am</H3>
-            <H1>Siyabonga Mazibuko</H1>
-            <H3>
-              <span>The web developer</span>
-            </H3>
-          </div>
-        </TextContainer>
-        <ImageContainer>
-          <ImageCover>
-            <img src={Hero} alt="hero" />
-          </ImageCover>
-        </ImageContainer>
-        <ActionContainer>
-          <div>
+          <ParagraphContainer>
             <P>
-              I am a very <span>passionate</span> web developer who specializes
-              in both <span>Front end & Back end</span>&nbsp;and is always
-              working hard &nbsp;<span>everyday</span> to improve his skills
+              Smart working passionate 23 year old full stack developer who
+              likes to do fun & creative projects & work with other developers
             </P>
-            <ButtonsCover>
-              <MyButton>My Projects</MyButton>
-              <MyButton>DownLoad Cv</MyButton>
-            </ButtonsCover>
-          </div>
-        </ActionContainer>
+          </ParagraphContainer>
+          <H3>I'm </H3>
+          <H1>Siyabonga Mazibuko</H1>
+          <H1>The Full Stack</H1>
+          <H1>
+            <span>Developer</span>
+          </H1>
+          <ButtonContainer>
+            <MyButton>Download Resume</MyButton>
+            <MyButton>See Projects</MyButton>
+          </ButtonContainer>
+        </TextContainer>
       </HomeContainer>
     </HomeWrapper>
   );
