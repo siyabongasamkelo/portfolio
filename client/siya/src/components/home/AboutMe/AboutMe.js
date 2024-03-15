4166c3ac5e9c9301d8104a709190e7e396e82b6d
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-14 17:12:16 -0700

    feat: improved about me page by using cards instead of straigh up paragraphs

--- client/siya/src/components/home/AboutMe/AboutMe.js
+++ client/siya/src/components/home/AboutMe/AboutMe.js
@@ -1,12 +1,111 @@
-import { H1, H3, P } from "../Home.styled";
-import { AboutMEPara, AboutMeText, AboutMeWrapper } from "./AboutMe.styled";
+import { H1, H3, H4, P } from "../Home.styled";
+import {
+  AboutCard,
+  AboutCardContainer,
+  AboutMEPara,
+  AboutMeText,
+  AboutMeWrapper,
+  ParaContainer,
+} from "./AboutMe.styled";
 
 const AboutMe = () => {
   return (
     <AboutMeWrapper>
       <AboutMeText>
         <H1>About Me</H1>
-        <AboutMEPara>
+        <AboutCardContainer>
+          <AboutCard>
+            <H4>1</H4>
+            <H4>How i got into coding ?</H4>
+            <ParaContainer>
+              <P>
+                My first language was visual basic back in 2019 when i was in
+                college ( Umfolozi TVET College ), i loved it so, learned about
+                :
+              </P>
+              <ul>
+                <li>Data types</li>
+                <li>Arrays</li>
+                <li>classes & Objects</li>
+                <li>OOP (Object Orientated Programming)</li>
+                <li>Database (My Sql)</li>
+              </ul>
+            </ParaContainer>
+          </AboutCard>
+
+          <AboutCard>
+            <H4>2</H4>
+            <H4>How i got into web development ?</H4>
+            <ParaContainer>
+              <P>
+                Then fast forward to 2021 i started pursuing web development and
+                i learned :
+              </P>
+              <ul>
+                <li>Html </li>
+                <li>Css</li>
+              </ul>
+              <P>i then pursued javascript in javascripti learned:</P>
+              <ul>
+                <li>ES6</li>
+                <li>Arrays</li>
+                <li>Promises</li>
+                <li>Callbacks</li>
+                <li>Arrow functions</li>
+                <li>async/await</li>
+                <li>Debugging and Troubleshooting</li>
+              </ul>
+            </ParaContainer>
+          </AboutCard>
+
+          <AboutCard>
+            <H4>3</H4>
+            <H4>Nodejs</H4>
+            <ParaContainer>
+              <P>
+                After coding in javascript for months i wanted to add extra
+                functionalities to my websites like storing data in Database and
+                maybe autheticating users so i learned nodejs :
+              </P>
+              <ul>
+                <li>Nodejs</li>
+                <li>Npm</li>
+                <li>Express</li>
+                <li>Restful Api</li>
+                <li>Authetication & Authorization</li>
+                <li>Server Side coding</li>
+                <li>Authetication & Authorization</li>
+                <li>Data Security & Protection</li>
+                <li>Code Reusability</li>
+                <li>PayPal</li>
+              </ul>
+            </ParaContainer>
+          </AboutCard>
+
+          <AboutCard>
+            <H4>4</H4>
+            <H4>Reactjs</H4>
+            <ParaContainer>
+              <P>
+                After getting the hang of nodejs i then started to explore
+                reactjs i was hearing a lot about it and it really did live up
+                to it expactations :
+              </P>
+              <ul>
+                <li>ReactJs</li>
+                <li>Styled-Components</li>
+                <li>Bootstrap</li>
+                <li>Redux</li>
+                <li>FrontEnd Design</li>
+                <li>Git</li>
+                <li>React Query</li>
+                <li>Formik & Yup</li>
+                <li>PayPal</li>
+              </ul>
+            </ParaContainer>
+          </AboutCard>
+        </AboutCardContainer>
+        {/* <AboutMEPara>
           <H3>How i got into programming</H3>
           <P>
             My first language was visual basic back in 2019 when i was in
@@ -78,7 +177,7 @@ const AboutMe = () => {
             <li>Formik & Yup</li>
             <li>PayPal</li>
           </ul>
-        </AboutMEPara>
+        </AboutMEPara> */}
       </AboutMeText>
     </AboutMeWrapper>
   );
