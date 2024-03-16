a00789c34035f378d1c9b188ed0a592bc4ca95e8
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-15 18:41:18 -0700

    fix: removed all projectcard styles to it own file to improve readability

--- client/siya/src/components/myprojects/MyProjects.styled.js
+++ client/siya/src/components/myprojects/MyProjects.styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { MyButton } from "../header/Header.styled";
 
 export const MyProjectWrapper = styled.section`
   margin-top: 10%;
@@ -8,47 +7,4 @@ export const MyProjectWrapper = styled.section`
 export const ProjectContainer = styled.div`
   margin-top: 5%;
   margin-left: 10%;
-`;
-
-export const ProjectCover = styled.div`
-  width: 90%;
-  padding: 30px;
-  border-radius: 20px;
-  background-color: #113946;
-  display: flex;
-
-  li {
-    color: rgba(225, 225, 225, 0.9);
-    /* text-align: left; */
-  }
-`;
-
-export const ButtonCover = styled.div`
-  width: 100%;
-  margin-top: 5%;
-`;
-
-export const GithubButton = styled(MyButton)`
-  background-color: #ffc47e;
-  margin-left: 5%;
-`;
-
-export const ProjectImage = styled.div`
-  width: 65%;
-  h4 {
-    margin-top: 5%;
-    color: #ffc47e;
-  }
-  img {
-    height: 40vh;
-    width: 100%;
-    border-radius: 10px;
-    object-fit: cover;
-  }
-`;
-
-export const ProjectData = styled.div`
-  width: 30%;
-  margin-left: 5%;
-  margin-top: 13%;
 `;
