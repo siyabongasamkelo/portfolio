cc807ec255e90b8ae9f4fe3c6fbd90cb2769cb5a
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-16 15:59:31 -0700

    feat: added watches & okuhleCrench website to the project lists

--- client/siya/src/components/myprojects/MyProjects.js
+++ client/siya/src/components/myprojects/MyProjects.js
@@ -1,7 +1,7 @@
 import { H1 } from "../home/Home.styled";
 import { MyProjectWrapper, ProjectContainer } from "./MyProjects.styled";
 import ProjectCard from "./ProjectCard";
-import { donuts } from "./ProjectDetails";
+import { donuts, okuhleCrech, watches } from "./ProjectDetails";
 
 const MyProjects = () => {
   return (
@@ -9,6 +9,8 @@ const MyProjects = () => {
       <H1>My Projects</H1>
       <ProjectContainer>
         <ProjectCard details={donuts} />
+        <ProjectCard details={watches} />
+        <ProjectCard details={okuhleCrech} />
       </ProjectContainer>
     </MyProjectWrapper>
   );
