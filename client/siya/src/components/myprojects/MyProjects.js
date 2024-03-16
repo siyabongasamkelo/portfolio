6f0971fcd03adbeae210b22359b75d7a7da069d5
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-15 18:40:14 -0700

    feat: made projectcard it won components to make code more cleaner

--- client/siya/src/components/myprojects/MyProjects.js
+++ client/siya/src/components/myprojects/MyProjects.js
@@ -1,65 +1,14 @@
-import { H1, H4, P } from "../home/Home.styled";
-import {
-  MyProjectWrapper,
-  ProjectContainer,
-  ProjectCover,
-  ProjectImage,
-  ProjectData,
-  ButtonCover,
-  GithubButton,
-} from "./MyProjects.styled";
-import donuts from "../img/donuts.png";
-import { ParaContainer } from "../home/AboutMe/AboutMe.styled";
-import { MyButton } from "../header/Header.styled";
-import { Globe, Github } from "react-bootstrap-icons";
+import { H1 } from "../home/Home.styled";
+import { MyProjectWrapper, ProjectContainer } from "./MyProjects.styled";
+import ProjectCard from "./ProjectCard";
+import { donuts } from "./ProjectDetails";
 
 const MyProjects = () => {
   return (
     <MyProjectWrapper>
       <H1>My Projects</H1>
       <ProjectContainer>
-        <ProjectCover>
-          <ProjectImage>
-            <H4>1) Mhlathuze/Siya Donuts</H4>
-            <ParaContainer style={{ margin: "0" }}>
-              <P>
-                An e-commerce web application that is dedicated for selling very
-                delicious donuts to the public
-              </P>
-            </ParaContainer>
-            <img src={donuts} alt="dunuts website" />
-            <ButtonCover>
-              <MyButton>
-                <Github /> Open Live site
-              </MyButton>
-              <GithubButton>
-                <Globe /> Open in GitHut
-              </GithubButton>
-            </ButtonCover>
-          </ProjectImage>
-          <ProjectData>
-            <H4>Features</H4>
-            <ul>
-              <li>Authetication System</li>
-              <li>Displaying Products From Database </li>
-              <li>Adding Products</li>
-              <li>Shopping Cart</li>
-              <li>Navigation</li>
-            </ul>
-            <H4>Stack</H4>
-            <ul>
-              <li>ReactJs</li>
-              <li>Styled Components </li>
-              <li>Formik & Yup </li>
-              <li>Bootstrap</li>
-              <li>FlowBite Ui</li>
-              <li>Redux Toolkit</li>
-              <li>Nodejs</li>
-              <li>Express</li>
-              <li>Mongodb</li>
-            </ul>
-          </ProjectData>
-        </ProjectCover>
+        <ProjectCard details={donuts} />
       </ProjectContainer>
     </MyProjectWrapper>
   );
