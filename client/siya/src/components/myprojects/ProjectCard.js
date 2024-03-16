c3ff3892a0ee5dfd82e1be461360acaa393db01b
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-15 18:42:11 -0700

    feat: project card that displays & deal with all projectcard logic

---
+++ client/siya/src/components/myprojects/ProjectCard.js
@@ -0,0 +1,58 @@
+import { ParaContainer } from "../home/AboutMe/AboutMe.styled";
+import { H4, P } from "../home/Home.styled";
+import {
+  ProjectCover,
+  ProjectImage,
+  ProjectData,
+  GithubButton,
+  ButtonCover,
+} from "./ProjectCard.styled";
+import { MyButton } from "../header/Header.styled";
+import { Globe, Github } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+
+const ProjectCard = ({ details }) => {
+  return (
+    <ProjectCover>
+      <ProjectImage>
+        <Link to={`${details.url}`} target={`_blank`}>
+          <H4>{details?.title}</H4>
+        </Link>
+        <ParaContainer style={{ margin: "0" }}>
+          <P>{details?.description}</P>
+        </ParaContainer>
+        <Link to={`${details.url}`} target={`_blank`}>
+          <img src={details.image} alt="dunuts website" />
+        </Link>
+        <ButtonCover>
+          <Link to={`${details.url}`} target={`_blank`}>
+            <MyButton>
+              <Globe /> visit site
+            </MyButton>
+          </Link>
+          <Link to={`${details.gitHub}`} target={`_blank`}>
+            <GithubButton>
+              <Github /> Open in GitHut
+            </GithubButton>
+          </Link>
+        </ButtonCover>
+      </ProjectImage>
+      <ProjectData>
+        <H4>Features</H4>
+        <ul>
+          {details?.features?.map((feature) => {
+            return <li>{feature}</li>;
+          })}
+        </ul>
+        <H4>Stack</H4>
+        <ul>
+          {details?.stack?.map((stack) => {
+            return <li>{stack}</li>;
+          })}
+        </ul>
+      </ProjectData>
+    </ProjectCover>
+  );
+};
+
+export default ProjectCard;
