6ebbdcab126a1d75661e43e25510aa42559e036e
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-15 18:44:23 -0700

    feat: moved all the project data & details to it own file to improve redability

---
+++ client/siya/src/components/myprojects/ProjectDetails.js
@@ -0,0 +1,28 @@
+import donut from "../img/donuts.png";
+
+export const donuts = {
+  features: [
+    "Authetication System",
+    "Displaying Products From Database",
+    "Adding Products",
+    "Shopping Cart",
+    "Navigation",
+  ],
+  stack: [
+    "ReactJs",
+    "Styled Components",
+    "Formik & Yup",
+    "Bootstrap",
+    "FlowBite Ui",
+    "Redux Toolkit",
+    "Nodejs",
+    "Express",
+    "Mongodb",
+  ],
+  description:
+    "An e-commerce web application that is dedicated for selling very delicious donuts to the public",
+  title: "1) Mhlathuze/Siya Donuts",
+  url: "https://siya-donuts.netlify.app/",
+  gitHub: "https://github.com/siyabongasamkelo/donuts",
+  image: donut,
+};
