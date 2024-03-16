c603c38635a4af24cb5c61a4eeee89e7f4250622
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-16 16:03:11 -0700

    feat: added watches & okuhleCrech to projectDetails

--- client/siya/src/components/myprojects/ProjectDetails.js
+++ client/siya/src/components/myprojects/ProjectDetails.js
@@ -1,10 +1,12 @@
 import donut from "../img/donuts.png";
+import watch from "../img/watches.png";
+import siphosakhe from "../img/siphosakhe.png";
 
 export const donuts = {
   features: [
     "Authetication System",
     "Displaying Products From Database",
-    "Adding Products",
+    "Adding Products to Database",
     "Shopping Cart",
     "Navigation",
   ],
@@ -25,4 +27,50 @@ export const donuts = {
   url: "https://siya-donuts.netlify.app/",
   gitHub: "https://github.com/siyabongasamkelo/donuts",
   image: donut,
+};
+
+export const watches = {
+  features: [
+    "Authetication System",
+    "Displaying Products From Database",
+    "Adding Products To Database",
+    "filturing,sorting & searching system",
+    "Writing reviews",
+    "Filturing & sorting reviews",
+    "sending Emails to users",
+    "Forgot password support",
+    "Shopping Cart",
+    "Accepting Payments with paypal",
+    "Navigation",
+  ],
+  stack: [
+    "ReactJs",
+    "Styled Components",
+    "Formik & Yup",
+    "Bootstrap",
+    "FlowBite Ui",
+    "Context",
+    "Nodejs",
+    "Express",
+    "Mongodb",
+    "Nodemailer",
+    "Paypal",
+  ],
+  description:
+    "An e-commerce web application that is for selling the worlds most luxurious watches",
+  title: "2) Luxurious Watches",
+  url: "https://siya-watches.netlify.app/",
+  gitHub: "https://github.com/siyabongasamkelo/watches",
+  image: watch,
+};
+
+export const okuhleCrech = {
+  features: ["Morden design", "Telling the story of a brand"],
+  stack: ["ReactJs", "Styled Components", "Bootstrap"],
+  description:
+    "A website for a local crech that is dedicated to guarding kids during the day",
+  title: "3) Siphosakhe crech",
+  url: "https://siphosakhecrech.netlify.app/",
+  gitHub: "https://github.com/siyabongasamkelo/okuhleCrech",
+  image: siphosakhe,
 };
