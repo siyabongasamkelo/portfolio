c00c29efbcca6ed0bb5291d9b75870f835a0558d
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-17 13:05:31 -0700

    feat: made myskills page responsive to all devices

--- client/siya/src/components/home/MySkills.styled.js
+++ client/siya/src/components/home/MySkills.styled.js
@@ -3,10 +3,29 @@ import { H4 } from "./Home.styled";
 
 export const MySkillsWrapper = styled.div`
   margin-top: 10%;
+  @media only screen and (max-width: 600px) {
+    text-align: center;
+  }
+  @media only screen and (min-width: 768px) {
+    text-align: center;
+  }
+  @media only screen and (min-width: 992px) {
+    text-align: left;
+  }
 `;
 
 export const MySkillCover = styled.div`
   display: flex;
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  }
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  @media only screen and (min-width: 992px) {
+    flex-wrap: nowrap;
+  }
 `;
 
 export const Stack = styled(H4)`
@@ -26,5 +45,20 @@ export const SkillCard = styled.div`
   li {
     color: rgba(225, 225, 225, 0.9);
     margin-left: 5%;
+    text-align: left;
+  }
+  @media only screen and (max-width: 600px) {
+    width: 95%;
+    margin-top: 5%;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 45%;
+    margin-top: 3%;
+    height: 35vh;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 25%;
+    margin-left: 2%;
+    height: 45vh;
   }
 `;
