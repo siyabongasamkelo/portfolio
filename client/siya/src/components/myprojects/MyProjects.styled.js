977c5ca77e106133651fb74b02755358f80c3a6d
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-17 13:13:04 -0700

    feat: made my project page responsive to all devices

--- client/siya/src/components/myprojects/MyProjects.styled.js
+++ client/siya/src/components/myprojects/MyProjects.styled.js
@@ -2,9 +2,23 @@ import styled from "styled-components";
 
 export const MyProjectWrapper = styled.section`
   margin-top: 10%;
+  @media only screen and (max-width: 600px) {
+    text-align: center;
+  }
+  @media only screen and (min-width: 768px) {
+    text-align: center;
+  }
+  @media only screen and (min-width: 992px) {
+    text-align: left;
+  }
 `;
 
 export const ProjectContainer = styled.div`
   margin-top: 5%;
   margin-left: 10%;
+  @media only screen and (min-width: 768px) {
+    margin-left: 0%;
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
