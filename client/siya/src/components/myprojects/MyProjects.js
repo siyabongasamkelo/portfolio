84a76625d89b899faca096bbe83b8a9d8098bb24
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-17 13:07:34 -0700

    feat: added paragraph to explain more details and added a link to my github for more projects

--- client/siya/src/components/myprojects/MyProjects.js
+++ client/siya/src/components/myprojects/MyProjects.js
@@ -1,12 +1,24 @@
-import { H1 } from "../home/Home.styled";
+import { ParaCover } from "../home/AboutMe/AboutMe.styled";
+import { H1, P } from "../home/Home.styled";
 import { MyProjectWrapper, ProjectContainer } from "./MyProjects.styled";
 import ProjectCard from "./ProjectCard";
 import { donuts, okuhleCrech, watches } from "./ProjectDetails";
+import { Link } from "react-router-dom";
 
 const MyProjects = () => {
+  const mygitHub = "https://github.com/siyabongasamkelo";
   return (
     <MyProjectWrapper>
       <H1>My Projects</H1>
+      <ParaCover>
+        <P>
+          These are a few of my best projects but if you're interested in seeing
+          more then please visit my
+          <Link to={mygitHub} target={`_blank`}>
+            git hub
+          </Link>
+        </P>
+      </ParaCover>
       <ProjectContainer>
         <ProjectCard details={donuts} />
         <ProjectCard details={watches} />
