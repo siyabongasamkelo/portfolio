394685b71560d4dc34a8cf950fd8b5e1f8068a37
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-18 15:22:19 -0700

    feat: header slider file

---
+++ client/siya/src/components/header/HeaderSlider.js
@@ -0,0 +1,39 @@
+import {
+  CloseButton,
+  HeaderSliderStyled,
+  LinksContainer,
+} from "./HeaderSlider.styled";
+import { X } from "react-bootstrap-icons";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+const HeaderSlider = ({ closeMenu, setCloseMenu, display }) => {
+  return (
+    <HeaderSliderStyled
+      as={motion.div}
+      animate={{
+        x: -35,
+        width: closeMenu ? 0 : 420,
+      }}
+      transition={{ duration: 0.5 }}
+      dis={display}
+    >
+      <CloseButton>
+        <X onClick={setCloseMenu} />
+      </CloseButton>
+      <LinksContainer onClick={setCloseMenu}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/shop"}>Shop</Link>
+        <Link to={"/Cart"}>Cart</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </LinksContainer>
+      {/* <Copyrights>
+        copyright&copy; {new Date().getFullYear()} All rights reserved siyabonga
+        Mazibuko
+      </Copyrights> */}
+    </HeaderSliderStyled>
+  );
+};
+
+export default HeaderSlider;
