c97b8c43cc041239e8817be0850c6249ab4a3a1a
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-18 15:11:05 -0700

    fix: changed the look made it better

--- client/siya/src/components/header/Header.js
+++ client/siya/src/components/header/Header.js
@@ -1,20 +1,40 @@
 import { HeaderWrapper, Logo, Links, MyButton, Action } from "./Header.styled";
 import { Link } from "react-router-dom";
-import { List } from "react-bootstrap-icons";
+import { Github, Instagram, List } from "react-bootstrap-icons";
+import { useState } from "react";
+import HeaderSlider from "./HeaderSlider";
+
 const Header = () => {
+  const [closeMenu, setCloseMenu] = useState(true);
+  const [display, setDisplay] = useState("block");
+
   return (
     <HeaderWrapper>
       <Logo>Siya</Logo>
       <Links>
         <Link to={"/"}>Home</Link>
+        <Link to={"/"}>About</Link>
         <Link to={"/"}>Projects</Link>
         <Link to={"/"}>Contact</Link>
-        <MyButton>My Projects</MyButton>
       </Links>
       <Action>
+        <Github className="alldevices" />
+        <Instagram className="alldevices" />
         <MyButton>My Projects</MyButton>
-        <List />
+        <List
+          onClick={() => {
+            closeMenu ? setDisplay("none") : setDisplay("block");
+            setCloseMenu(false);
+          }}
+        />
       </Action>
+      <HeaderSlider
+        setCloseMenu={() => {
+          setCloseMenu(true);
+        }}
+        closeMenu={closeMenu}
+        display={display}
+      ></HeaderSlider>
     </HeaderWrapper>
   );
 };
