c9a061dd7af8caca4775af193098f564a4cf1b36
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-20 14:43:21 -0700

    feat: used validator to check if email is valid

--- server/Index.js
+++ server/Index.js
@@ -2,25 +2,24 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import validator from "validator";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.all("*", (req, res) => {
-//   res.status(400).json("404 Page not found");
-// });
-
 app.post("/send-message", (req, res) => {
   try {
     const myEmail = process.env.MYEMAIL;
     const { message, email } = req.body;
 
-    console.log(req.body);
-
     if (!message) return res.status(400).json("message is required");
     if (!email) return res.status(400).json("email is required");
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("invalid email");
+    }
 
     const transporter = nodemailer.createTransport({
       service: "hotmail",
@@ -33,7 +32,7 @@ app.post("/send-message", (req, res) => {
     const options = {
       from: "siyabongasamkelociam@outlook.com",
       to: `${myEmail}`,
-      subject: "Luxurious Watches Subscription Email",
+      subject: "Message from your personal website",
       text: `Hellow syah someone sent you a message from your website that says ${message}`,
     };
 
@@ -48,6 +47,10 @@ app.post("/send-message", (req, res) => {
     console.log(err);
     res.status(400).json(err);
   }
+});
+
+app.all("*", (req, res) => {
+  res.status(400).json("404 Page not found");
 });
 
 const PORT = process.env.PORT || 5000;
