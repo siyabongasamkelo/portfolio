73f608f890fa4d054db7549b3fe82e6697820f74
siyabonga <siyabongasamkelociam@gmail.com>
2024-03-20 14:29:59 -0700

    feat: added hash link to scroll to contact section on button click

--- client/siya/src/components/footer/Footer.js
+++ client/siya/src/components/footer/Footer.js
@@ -14,6 +14,7 @@ import {
   Twitter,
   Whatsapp,
 } from "react-bootstrap-icons";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Footer = () => {
   return (
@@ -22,7 +23,9 @@ const Footer = () => {
         <div>
           <H4>Got a project in mind that you want us to work on ...?</H4>
         </div>
-        <MyButton>Let's Do It</MyButton>
+        <Link to={"#contacts"}>
+          <MyButton>Let's Do It</MyButton>
+        </Link>
       </LetWorkTogether>
       <SocialMediaIcons>
         <Facebook />
